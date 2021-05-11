0344eec0382c2d5b38004a6f93e048795d3cbd63
alvaropp99 <73893659+alvaropp99@users.noreply.github.com>
2021-05-11 20:48:41 +0200

    se ha añadido el menú del listFragment y modificado borde de los campos de la nota

--- app/src/main/java/com/example/taskid/fragments/list/ListFragment.kt
+++ app/src/main/java/com/example/taskid/fragments/list/ListFragment.kt
@@ -1,10 +1,8 @@
 package com.example.taskid.fragments.list
 
 import android.os.Bundle
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import androidx.navigation.fragment.findNavController
 import com.example.taskid.R
 import com.example.taskid.databinding.FragmentListBinding
@@ -19,17 +17,27 @@ class ListFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
+        // mediante el binding se enlaza la clase de kotlin con su respectivo layout y atributos
         _binding= FragmentListBinding.inflate(inflater, container, false)
 
+
+        //cuando se pulsa el floating button que abra el fragment que añade una nota
         binding.addTaskButton.setOnClickListener{
             findNavController().navigate(R.id.action_listFragment_to_addFragment)
         }
 
+        //te lleva al fragment que permite editar una nota
         binding.listLayout.setOnClickListener{
             findNavController().navigate(R.id.action_listFragment_to_updateFragment)
         }
+
+        //fijamos el menú list_fragment_menu
+        setHasOptionsMenu(true)
+
         return binding.root
     }
 
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.list_fragment_menu, menu)
+    }
 }
