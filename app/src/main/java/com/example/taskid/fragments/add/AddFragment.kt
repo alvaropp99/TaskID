792768aac1eceedad141fee092b78f7a9ebe2839
alvaropp99 <73893659+alvaropp99@users.noreply.github.com>
2021-05-13 21:17:21 +0200

    menús acabados y animaciones listas

--- app/src/main/java/com/example/taskid/fragments/add/AddFragment.kt
+++ app/src/main/java/com/example/taskid/fragments/add/AddFragment.kt
@@ -1,10 +1,8 @@
 package com.example.taskid.fragments.add
 
 import android.os.Bundle
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import com.example.taskid.R
 
 class AddFragment : Fragment() {
@@ -13,7 +11,14 @@ class AddFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
+
+        //fijamos el menú list_fragment_menu
+        setHasOptionsMenu(true)
+
         return inflater.inflate(R.layout.fragment_add, container, false)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.add_fragment_menu, menu)
     }
 }
--- app/src/main/java/com/example/taskid/fragments/update/UpdateFragment.kt
+++ app/src/main/java/com/example/taskid/fragments/update/UpdateFragment.kt
@@ -1,10 +1,8 @@
 package com.example.taskid.fragments.update
 
 import android.os.Bundle
+import android.view.*
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
 import com.example.taskid.R
 
 class UpdateFragment : Fragment() {
@@ -14,6 +12,14 @@ class UpdateFragment : Fragment() {
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
+
+        //fijamos el menú list_fragment_menu
+        setHasOptionsMenu(true)
+
         return inflater.inflate(R.layout.fragment_update, container, false)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.update_fragment_menu, menu)
     }
 }
